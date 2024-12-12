30ff965c2a3caec3db5b7f8a292203b2ab9f2818
VisigothKing1 <20221926@novaims.unl.pt>
2024-12-12 16:15:59 +0000

    bug fixes.4.0 and wallet

--- rounds.py
+++ rounds.py
@@ -12,7 +12,7 @@ class Rounds:
             enemy.speed += current_round // 2
             enemy.health += int(10 + current_round * 2.5)  # Slower scaling
 
-    def pre_round_countdown(self, screen, font, player):
+    def pre_round_countdown(self, screen, font, player, power_timer, power_timer2, power_timer3, power_timer4):
         # Move player to the shed location
         shed_exploration = True
         shed_text = font.render(
@@ -34,9 +34,30 @@ class Rounds:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RETURN:  # Exit shed and prepare for countdown
                         shed_exploration = False
+                        if power_timer and power_timer.paused:
+                            power_timer.resume()
+
+                        if power_timer2 and power_timer2.paused:
+                            power_timer2.resume()
+
+                        if power_timer3 and power_timer3.paused:
+                            power_timer3.resume()
+
+                        if power_timer4 and power_timer4.paused:
+                            power_timer4.resume()
+
                     elif event.key == pygame.K_p:  # Go to shed
                         shed(player)
+                        if power_timer and not power_timer.paused:
+                            power_timer.pause()
 
+                        if power_timer2 and not power_timer2.paused:
+                            power_timer2.pause()
+
+                        if power_timer3 and not power_timer3.paused:
+                            power_timer3.pause()
+                        if power_timer4 and not power_timer4.paused:
+                            power_timer4.pause()
 
         # Countdown before the next round starts
         for i in range(3, 0, -1):
