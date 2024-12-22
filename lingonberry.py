805186d08cf3b06fbab1c163da734477e4479984
Lukas-a7 <lu0753an-s@student.lu.se>
2024-12-22 16:32:31 +0100

    Big push

---
+++ lingonberry.py
@@ -0,0 +1,14 @@
+from weapon import Weapon
+from bullet_lingonberry import BulletLingonberry
+
+class Lingonberry(Weapon):
+    def __init__(self):
+        super().__init__(name="Lingonberry", damage=3, range=200, cooldown=0, attack_speed=50)
+
+    def fire(self, x, y, direction):
+        if self.cooldown <= 0:
+            self.cooldown = self.attack_speed
+            return BulletLingonberry(x, y, direction)
+        else:
+            self.cooldown -= 1
+            return None
